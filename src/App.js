bd72836632bca488e5f3660c427600d679f19006
GDIV <gdavisiv@gmail.com>
2022-06-10 14:13:48 -0400

    BASE: Updated and Running

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//You can easily build out this file with es7snippets addon
+//Type 'rafce' to build it quickly
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div>
+        <h1>GDIV</h1>
+    </div>
+  )
+  
+}
+
+export default App
