2303fd7097debe08488c5c2b08a0971ce9895a03
GDIV <gdavisiv@gmail.com>
2022-06-10 13:57:18 -0400

    Starting from Scratch

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ReactDOM } from 'react-dom';
-
-import App from './App';
-
-//This react compenent will be rendered inside the public/index.html file
-//with an ID of 'root'
-ReactDOM.render(<App />, document.getElementById('root'));
