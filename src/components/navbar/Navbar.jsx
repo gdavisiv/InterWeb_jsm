082e29fecdc89ba0bc8f26b86e2b276f22eabfbb
GDIV <gdavisiv@gmail.com>
2022-06-13 00:45:08 -0400

    Update: Buildout for Menu Bar; Added Assetsl; fixed case issue in package.json

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/logo.svg'
 import './navbar.css';
 
+//BEM -> Block Element Modifier
 const Navbar = () => {
   return (
-    <div>
-        Navbar
+    <div className="3dm__navbar">
+      <div className="3dm__navbar-links">
+        <div className="3dm__navbar-links_logo">
+          <img src={logo} alt="Logo" />
+        </div>
+      </div>
     </div>
   )
 }
