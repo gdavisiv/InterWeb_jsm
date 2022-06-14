4c96e1840fafd07ec8a7f274c1c2ec661e8e60f1
GDIV <gdavisiv@gmail.com>
2022-06-14 00:01:41 -0400

    Updating Navbar css/jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 //BEM -> Block Element Modifier
 const Navbar = () => {
   return (
-    <div className="3dm__navbar">
-      <div className="3dm__navbar-links">
-        <div className="3dm__navbar-links_logo">
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="Logo" />
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#what3DM">What is GPT3?</a></p>
+          <p><a href="#possibility">OpenAI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
       </div>
     </div>
   )
