a0be4ca395c64907dc8ae1ae595ac91dff6d6636
GDIV <gdavisiv@gmail.com>
2022-06-18 22:32:02 -0400

    Header: Updates to CSS for mobile view

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,9 @@ const Header = () => {
     <div className= "gpt3__header section__padding" id="Home">
       <div className='gpt3__header-content'>
         <h1 className="gradient__text">We turn your vision into tangible results with GPT-3 OpenAI</h1>
-        <p>Over 300 applications are delivering GPT-3–powered search, conversation, text completion, and other advanced AI features through our API.</p>
+        <p>Over 300 applications are delivering GPT-3–powered search, conversation, text completion, and other advanced AI features through our API.
+        Developers can now fine-tune GPT-3 on their own data, creating a custom version tailored to their application. Customizing makes GPT-3 reliable for a wider variety of use cases and makes running the model cheaper and faster.
+        </p>
         
         <div className="gpt3__header-content__input">
           <input type=""email placeholder="Your Email Adddress" />
@@ -16,13 +18,13 @@ const Header = () => {
         </div>
 
         <div className="gpt3__header-content__people">
-          <img src={people} alt="Photo of people" />
+          <img src={people} alt-text="Photo of people who have accessed GPT-3 in the last 24 hours!" />
         </div>
         <p>1,600 people requested access to GPT-3 in the last 24 hours</p>
 
-{/*         <div className="gpt3__header-image">
-          <img src={ai} alt="Artist rendering of AI" />
-        </div> */}
+        <div className="gpt3__header-image">
+          <img src={ai} alt-text="Artist rendering of what an Artificial Intelligence face would resemble." />
+        </div>
 
       </div>
     </div>
