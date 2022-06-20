69f387b3b06b4eb0ce894d2e72a8e265b927cbb3
GDIV <gdavisiv@gmail.com>
2022-06-19 23:02:55 -0400

    BRAND: building out brands section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { google, slack, dropbox, atlassian, shopify } from './imports';
 import './brand.css';
 
 const Brand = () => {
   return (
-    <div>
+    <div class='gtp3__brand section__padding'>
         Brand
     </div>
   )
