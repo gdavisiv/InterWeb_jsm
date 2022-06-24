cba60dc87fe39418ee6b5b9f100f6c6a72432388
GDIV <gdavisiv@gmail.com>
2022-06-24 10:48:44 -0400

    Brand: Styling Completed

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+//Helper line to import out assets easily
 import { google, slack, dropbox, atlassian, shopify } from './imports';
 import './brand.css';
 
-const Brand = () => {
-  return (
-    <div className='gtp3__brand section__padding'>
-        <div>
-          <img src={google} alt="Google Logo" />
-        </div>
-        <div>
-          <img src={slack} alt="Slack Logo" />
-        </div>
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt="Google Logo" />
     </div>
-  )
-}
+    <div>
+      <img src={slack} alt="Slack Logo" />
+    </div>
+    <div>
+      <img src={atlassian} alt="Atlassian Logo" />
+    </div>
+    <div>
+      <img src={dropbox} alt="Dropbox Logo" />
+    </div>
+    <div>
+      <img src={shopify} alt="Shopify Logo"/>
+    </div>
+  </div>
+);
 
-export default Brand
+export default Brand;
