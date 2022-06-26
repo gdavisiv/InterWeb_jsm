86140bcc3934c560688a1cb937079cdccc9b1987
GDIV <gdavisiv@gmail.com>
2022-06-25 20:53:23 -0400

    WhatGPT3: Creating Next Section

--- src/containers/what3DM/What3DM.jsx
+++ src/containers/what3DM/What3DM.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './what3DM.css';
 
 const What3DM = () => {
   return (
-    <div>
-        What3DM
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">The endless applications are beyond your imagination!</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-component">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
