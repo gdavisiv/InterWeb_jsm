5df26d03d42a2a3da620be4579b063aaee55a983
GDIV <gdavisiv@gmail.com>
2022-07-03 00:11:50 -0400

    Features: Updating with Text/Classes

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,8 +3,11 @@ import './features.css';
 
 const Features = () => {
   return (
-    <div>
-        Features
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">The Future is Now.  Position your business on the cutting edge of AI Technology powered by GPT-3.</h1>
+        <p>Request Early Access to get started Today!</p>
+      </div>
     </div>
   )
 }
