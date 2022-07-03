5024c877acf74ddf166b80cc479deebc24ba2676
GDIV <gdavisiv@gmail.com>
2022-07-03 00:16:42 -0400

    Created featuresData

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import './features.css';
 
+const featuresData = [
+  {
+    title: 'Lorum Impsum',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Tristique senectus et netus et malesuada.',
+  },
+  {
+    title: 'Lorum Impsum',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Tristique senectus et netus et malesuada.',
+  },
+  {
+    title: 'Lorum Impsum',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Tristique senectus et netus et malesuada.',
+  },
+  {
+    title: 'Lorum Impsum',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Tristique senectus et netus et malesuada.',
+  },
+];
+
+
 const Features = () => {
   return (
     <div className="gpt3__features section__padding" id="features">
@@ -8,6 +28,9 @@ const Features = () => {
         <h1 className="gradient__text">The Future is Now.  Position your business on the cutting edge of AI Technology powered by GPT-3.</h1>
         <p>Request Early Access to get started Today!</p>
       </div>
+        <div className="gpt3__features-container">
+
+        </div>
     </div>
   )
 }
