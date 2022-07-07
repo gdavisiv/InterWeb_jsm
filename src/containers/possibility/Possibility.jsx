813df66fc5b557ada00fb73fad578f675233cc43
GDIV <gdavisiv@gmail.com>
2022-07-07 17:25:56 -0400

    Fixed className errors and completed CSS

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -10,7 +10,7 @@ const Possibility = () => {
       </div>
       <div className="gpt3__possibility-content">
         <h4>Request early access to get started.</h4>
-        <h1 className="gradient-text">The possibilities are endless, turn your company vision into tangible results!</h1>
+        <h1 className="gradient__text">The possibilities are endless, turn your company vision into tangible results!</h1>
         <p>Get started today, and one of our skilled professional will contact you at your earlist convenience.  We strive to make sure your transiition will be a smooth and painless process.</p>
         <h4>Click here to schedule your consultation.</h4>
       </div>
