3b57a5b229fe6f673de40379180874a40ed7f71e
GDIV <gdavisiv@gmail.com>
2022-07-07 17:06:26 -0400

    Building out Possibility Section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import possibilityImage from '../../assets/possibility.png';
 import './possibility.css';
 
 const Possibility = () => {
   return (
-    <div>
-        Possibility
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="Future Possibilities with GPT-3"></img>
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request early access to get started.</h4>
+        <h1 className="gradient-text">The possibilities are endless, turn your company vision into tangible results!</h1>
+        <p>Get started today, and one of our skilled professional will contact you at your earlist convenience.  We strive to make sure your transiition will be a smooth and painless process.</p>
+        <h4>Click here to schedule your consultation.</h4>
+      </div>
     </div>
   )
 }
