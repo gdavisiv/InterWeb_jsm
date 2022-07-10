1f5d7bc09ab9a1a1962a4dd5be27748d4457a6bc
GDIV <gdavisiv@gmail.com>
2022-07-10 17:02:58 -0400

    Built Footer JS/CSS

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="gpt3__footer section__padding">
       <div className="gpt3__footer-heading">
-        <h1 className="gadient__text">Step into the future with our suite of tools.</h1>
+        <h1 className="gradient__text">Step into the future with our suite of tools.</h1>
       </div>
         <div className="gpt3__footer-btn">
             <p>Request Early Access</p>
@@ -14,9 +14,32 @@ const Footer = () => {
         <div className="gpt3__footer-links">
           <div className="gpt3__footer-links_logo">
             <img src={gpt3Logo} alt="logo" />
-            <p>New York, NY</p>
+            <p>3DMations, All Rights Reserved</p>
           </div>
-            <div className="gpt3__footer-links_div"></div>
+            <div className="gpt3__footer-links_div">
+              <h4>New to GPT-3?</h4>
+              <p>What is an AI-Chatbot</p>
+              <p>What is 3DMations</p>
+              <p>Customer Service</p>
+              <p>Versions</p>
+            </div>
+            <div className="gpt3__footer-links_div">
+              <h4>About 3DMations</h4>
+              <p>Our Story</p>
+              <p>Blog</p>
+              <p>Careers</p>
+              <p>Give us your feedback</p>
+            </div>
+            <div className="gpt3__footer-links_div">
+              <h4>Popular Links</h4>
+              <p>New Release Features</p>
+              <p>3DM for Startups</p>
+              <p>3DM LIVE</p>
+              <p>Newsletter Sign-Up</p>
+            </div>
+        </div>
+        <div className="gpt3__footer-copyright">
+          <p>© Copyright 2022 3DMations.  All Rights Reserved</p>
         </div>
     </div>
   )
