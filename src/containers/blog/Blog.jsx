9c7807455e8d87d2c5b672d99680b87de41dbc17
GDIV <gdavisiv@gmail.com>
2022-07-10 13:58:43 -0400

    Blog: Customized CSS for screensizes

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -11,13 +11,13 @@ const Blog = () => {
       </div>
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container_groupA">
-          <Article imgUrl={blog01} date="May 23, 2022" text = "Overview: The OpenAI API is powered by a family of models with different capabilities and price points." />
+          <Article imgUrl={blog01} date="May 23, 2022" text = "Overview: OpenAI API is powered by a family of models with different capabilities and price points." />
         </div>
         <div className="gpt3__blog-container_groupB">
           <Article imgUrl={blog02} date="April 5, 2022" text = "What is GPT3: Our models can understand and generate.." />
           <Article imgUrl={blog03} date="Feb 17, 2022" text = "How to use Ada: usually the fastest model and can perform.." />
           <Article imgUrl={blog04} date="Jan 29, 2022" text = "Feature-specific models: The main GPT-3 models are meant to be.." />
-          <Article imgUrl={blog05} date="Jan 11, 2022" text = "What is Curie: Curie is extremely powerful, yet very fast. While.." />
+          <Article imgUrl={blog05} date="Jan 11, 2022" text = "What is Curie: Curie is extremely powerful, yet very fast. While providing.." />
         </div>
       </div>
     </div>
