5929908137b5fe35b4f45e3b5ee9e251c3d6ae4b
GDIV <gdavisiv@gmail.com>
2022-07-10 09:58:18 -0400

    Blog: Completed FINAL version

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './article.css';
 
-const Article = ({ imgUrl, date, title }) => {
+const Article = ({ imgUrl, date, text }) => {
   return (
-    <div className="gpt__blog-container_article">
+    <div className="gpt3__blog-container_article">
       <div className="gpt3__blog-container_article-image">
         <img src={imgUrl} alt="blog" />
       </div>
       <div className="gpt3__blog-container_article-content">
         <div>
           <p>{date}</p>
-          <h3>{title}</h3>
+          <h3>{text}</h3>
         </div>
-        <p>Read the full Article.</p>
+        <p>Read the Full Article</p>
       </div>
     </div>
   )
 }
 
-export default Article
+export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -7,21 +7,21 @@ const Blog = () => {
   return (
     <div className="gpt3__blog section__padding" id="blog">
       <div className="gpt3__blog-heading">
-        <h1 className="gradient__text">Troubleshooting and how-to tips for GPT3 products</h1>
+        <h1 className="gradient__text">Troubleshooting <br /> and how-to tips with our Knowledgebase</h1>
       </div>
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container_groupA">
-          <Article imgUrl={blog01} date="May 23, 2022" title="Overview: The OpenAI API is powered by a family of models with different capabilities and price points." />
+          <Article imgUrl={blog01} date="May 23, 2022" text = "Overview: The OpenAI API is powered by a family of models with different capabilities and price points." />
         </div>
         <div className="gpt3__blog-container_groupB">
-          <Article imgUrl={blog02} date="April 5, 2022" title="What is GPT3: Our models can understand and generate natural language." />
-          <Article imgUrl={blog03} date="Feb 17, 2022" title="How to use Ada: usually the fastest model and can perform tasks like parsing text." />
-          <Article imgUrl={blog04} date="Jan 29, 2022" title="Feature-specific models: The main GPT-3 models are meant to be used with the text completion endpoint" />
-          <Article imgUrl={blog05} date="Jan 11, 2022" title="What is Curie: Curie is extremely powerful, yet very fast. While Davinci is stronger when it comes to analyzing"/>
+          <Article imgUrl={blog02} date="April 5, 2022" text = "What is GPT3: Our models can understand and generate.." />
+          <Article imgUrl={blog03} date="Feb 17, 2022" text = "How to use Ada: usually the fastest model and can perform.." />
+          <Article imgUrl={blog04} date="Jan 29, 2022" text = "Feature-specific models: The main GPT-3 models are meant to be.." />
+          <Article imgUrl={blog05} date="Jan 11, 2022" text = "What is Curie: Curie is extremely powerful, yet very fast. While.." />
         </div>
       </div>
     </div>
   )
 }
 
-export default Blog
+export default Blog;
